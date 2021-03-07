eb3789abcd3a885cd31332467ccf0a58e32dc132
Marko Tribl <triblmarko@gmail.com>
2021-03-07 21:11:48 +0100

    Refactor test to remove duplication

--- WeatherAppTests/RemoteWeatherLoaderTests.swift
+++ WeatherAppTests/RemoteWeatherLoaderTests.swift
@@ -29,17 +29,14 @@ protocol HTTPClient {
 class RemoteWeatherLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://any-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteWeatherLoader(url: url, client: client)
+        let (client, _) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let url = URL(string: "https://any-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteWeatherLoader(url: url, client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        let (client, sut) = makeSUT(url: url)
         
         sut.load()
         
@@ -56,4 +53,11 @@ class RemoteWeatherLoaderTests: XCTestCase {
             requestedURL = url
         }
     }
+    
+    private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (client: HTTPClientSpy, sut: RemoteWeatherLoader) {
+        let client = HTTPClientSpy()
+        let sut = RemoteWeatherLoader(url: url, client: client)
+        return (client, sut)
+    }
+    
 }
