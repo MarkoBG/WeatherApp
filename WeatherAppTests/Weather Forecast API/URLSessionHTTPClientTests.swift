f960eef15271d9530158220a4986a95766277636
Marko Tribl <triblmarko@gmail.com>
2021-03-27 12:00:15 +0100

    Remove redundant test

--- WeatherAppTests/Weather Forecast API/URLSessionHTTPClientTests.swift
+++ WeatherAppTests/Weather Forecast API/URLSessionHTTPClientTests.swift
@@ -21,17 +21,6 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
 
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "https://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURLs, [url])
-        
-    }
-    
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "https://any-url.com")!
         let session = URLSessionSpy()
@@ -50,7 +39,6 @@ class URLSessionHTTPClientTests: XCTestCase {
     //MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
-        var receivedURLs = [URL]()
         var capturedTask = [URL: URLSessionDataTask]()
         
         func stub(url: URL, task: URLSessionDataTask) {
@@ -58,8 +46,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
-            
             return capturedTask[url] ?? FakeURLSessionDataTask()
         }
     }
