1de904f86e1d65d78baaa3ec34e1e7752fac6326
Marko Tribl <triblmarko@gmail.com>
2021-05-27 23:14:55 +0200

    Move 'LocalWeatherLoader' and 'WeatherForecastStore' collaborator to its own file in production

--- WeatherAppTests/Weather Forecast Cache/CacheWeatherForecastUseCase.swift
+++ WeatherAppTests/Weather Forecast Cache/CacheWeatherForecastUseCase.swift
@@ -8,41 +8,7 @@
 import XCTest
 import WeatherApp
 
-class LocalWeatherLoader {
-    private let store: WeatherForecastStore
-    private let currentDate: () -> Date
-    
-    init(store: WeatherForecastStore, currentDate: @escaping () -> Date) {
-        self.store = store
-        self.currentDate = currentDate
-    }
-    
-    func save(_ forecast: WeatherForecast, completion: @escaping (Error?) -> Void) {
-        store.deleteCachedWeatherForecast { [weak self] error in
-            guard let self = self else { return }
-            
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.cache(forecast, with: completion)
-            }
-        }
-    }
-    
-    private func cache(_ forecast: WeatherForecast, with completion: @escaping (Error?) -> Void) {
-        store.insert(forecast, timestamp: currentDate()) { [weak self] error in
-            guard self != nil else { return }
-            completion(error)
-        }
-    }
-}
 
-protocol WeatherForecastStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
-    func deleteCachedWeatherForecast(completion: @escaping DeletionCompletion)
-    func insert(_ forecast: WeatherForecast, timestamp: Date, completion: @escaping InsertionCompletion)
-}
 
 class CacheWeatherForecastUseCase: XCTestCase {
 
